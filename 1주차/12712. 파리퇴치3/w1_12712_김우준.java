882e0287ebfc9dab0692c3925a45faa0f3f67c16
woojun-k <kwoojun.eng@gmail.com>
2025-01-18 17:46:15 +0900

    add: solution for SWEA #12712

--- 1주차/12712. 파리퇴치3/w1_12712_김우준.java
+++ 1주차/12712. 파리퇴치3/w1_12712_김우준.java
@@ -2,12 +2,12 @@ import java.util.Scanner;
 
 public class w1_12712_김우준 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
+        Scanner sc = new Scanner(System.in);
         int T = Integer.parseInt(sc.nextLine());
 
         for (int test_case = 0; test_case < T; test_case++) {
-        	String[] str = sc.nextLine().split(" ");
+            String[] str = sc.nextLine().split(" ");
             int N = Integer.parseInt(str[0]);
             int M = Integer.parseInt(str[1]);
             int[][] arr = new int[N][N];
@@ -20,23 +20,31 @@ public class w1_12712_김우준 {
             }
 
             int result = 0;
-            
+
             for (int i = 0; i < N; i++) {
                 for (int j = 0; j < N; j++) {
                     int plus = arr[i][j];
                     int cross = arr[i][j];
                     int max = 0;
-                   
+
                     for (int m = 1; m < M; m++) {
-                    	if(i + m < N) plus += arr[i + m][j];
-                    	if(i - m >= 0) plus += arr[i - m][j];
-                    	if(j + m < N) plus += arr[i][j + m];
-                    	if(j - m >= 0) plus += arr[i][j - m];
-                    	
-                    	if(i + m < N && j + m < N) cross += arr[i + m][j + m];
-                    	if(i + m < N && j - m >= 0) cross += arr[i + m][j - m];
-                    	if(i - m >= 0 && j + m < N) cross += arr[i - m][j + m];
-                    	if(i - m >= 0 && j - m >= 0) cross += arr[i - m][j - m];
+                        if (i + m < N)
+                            plus += arr[i + m][j];
+                        if (i - m >= 0)
+                            plus += arr[i - m][j];
+                        if (j + m < N)
+                            plus += arr[i][j + m];
+                        if (j - m >= 0)
+                            plus += arr[i][j - m];
+
+                        if (i + m < N && j + m < N)
+                            cross += arr[i + m][j + m];
+                        if (i + m < N && j - m >= 0)
+                            cross += arr[i + m][j - m];
+                        if (i - m >= 0 && j + m < N)
+                            cross += arr[i - m][j + m];
+                        if (i - m >= 0 && j - m >= 0)
+                            cross += arr[i - m][j - m];
                     }
                     max = (plus > cross) ? plus : cross;
                     result = (max > result) ? max : result;
