f77873ebb71dc14ebb6240352a7a61bd838a4dd8
woojun-k <kwoojun.eng@gmail.com>
2025-01-21 00:54:32 +0900

    add: solution for SWEA #1868

--- 1주차/1868. 파핑파핑 지뢰찾기/w1_1868_김우준.java
+++ 1주차/1868. 파핑파핑 지뢰찾기/w1_1868_김우준.java
@@ -49,9 +49,12 @@ public class w1_1868_김우준 {
 
         for (int test_case = 0; test_case < T; test_case++) {
             int N = Integer.parseInt(sc.nextLine());
+            // N * N 크기의 배열 생성
+            // N * N 크기의 flag 배열 생성
             String[][] arr = new String[N][N];
             boolean[][] flag_arr = new boolean[N][N];
 
+            // 배열에 문자할당 flag 배열 초기화
             for (int i = 0; i < N; i++) {
                 String[] str = sc.nextLine().split("");
                 for (int j = 0; j < N; j++) {
@@ -63,16 +66,22 @@ public class w1_1868_김우준 {
             int click = 0;
             for (int i = 0; i < N; i++) {
                 for (int j = 0; j < N; j++) {
+                    // 만약 [i][j] 요소에 한번도 접근한 적이 없고, [i][j] 요소가 "." 이라면 calc 메서드 수행
+                    // calc메서드는 주변 8칸의 지뢰 갯수를 탐색 후 지뢰 개수를 반환
                     if (flag_arr[i][j] && arr[i][j].equals(".")) {
                         int count = calc(N, i, j, arr);
+                        // 만약 탐색된 지뢰가 없다면 check 메서드를 수행하여 flag[i][j]를 방문으로 체크
+                        // queue에 [i, j]추가 및 주변 8칸에 지뢰가 탐색될 때 까지 queue에 [i+x][j+y]를 추가하여 재귀적으로 탐색
                         if (count == 0) {
                             check(N, i, j, arr, flag_arr);
+                            // 모든 인접한 안전지대를 확인 후 클릭 수 1 증가
                             click++;
                         }
                     }
                 }
             }
 
+            // 모든 인접한 안전지대를 제외한 나머지 "." 확인을 위한 클릭 횟수 추가
             for (int i = 0; i < N; i++) {
                 for (int j = 0; j < N; j++) {
                     if (arr[i][j].equals(".")) {
