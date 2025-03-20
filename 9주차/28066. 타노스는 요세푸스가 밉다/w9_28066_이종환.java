32cfd558206974028dba75cc66cc0e417b8206f2
0224LJH <kavae550@gmail.com>
2025-03-20 18:56:05 +0900

    add:solution for BOJ 28066

--- 9주차/28066. 타노스는 요세푸스가 밉다/w9_28066_이종환.java
+++ 9주차/28066. 타노스는 요세푸스가 밉다/w9_28066_이종환.java
@@ -1,4 +1,31 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
 
-public class w9_28066_이종환 {
-
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int totalCnt = sc.nextInt();
+		int perDelete = sc.nextInt();
+		Queue<Integer> q = new LinkedList<>();
+		
+		for (int i = 1; i <= totalCnt; i++) {
+			q.add(i);
+		}
+		
+		while(true) {
+			int first = q.poll();
+			
+			for (int i = 1; i < perDelete; i++) {
+				if (q.isEmpty()) break;
+				q.poll();
+			}
+			
+			if (q.size() == 0) {
+				System.out.println(first);
+				break;
+			}
+			q.add(first);
+		}
+	}
 }
