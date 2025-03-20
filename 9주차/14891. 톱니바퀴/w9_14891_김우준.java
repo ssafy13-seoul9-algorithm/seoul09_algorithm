29bf5a2628e9878a4648c873e567fbb42985aef1
woojun-k <kwoojun.eng@gmail.com>
2025-03-20 17:01:55 +0900

    add: solution for Baekjoon #14891

---
+++ 9주차/14891. 톱니바퀴/w9_14891_김우준.java
@@ -0,0 +1,12 @@
+// 뭔가 deque로 시계 방향으로 돌리거나
+// 반시계 방향으로 first, last poll push
+// 톱니바퀴 인덱스도 알아야하니 List<Deque<Char>> 이렇게 하면 될듯
+// 현재 돌리려고 하는 위치의 톱니바퀴의 3번째, 7번째와, +-1 인덱스의 톱니바퀴의 맞은편 값 확인
+// 값이 다르면 돌리는 방향과 돌려야하는 인덱스 queue로 별도 관리 push
+// 돌렸다면, 또 해당 위치에서 +-1 인덱스 검사
+
+public class App {
+  public static void main(String[] args) {
+    // 압도적 사죄!!
+  }
+}
