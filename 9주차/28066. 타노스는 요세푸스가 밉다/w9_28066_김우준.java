27a25f87e7ffebf9952c4ed9221dcda0b8e2d8b7
woojun-k <kwoojun.eng@gmail.com>
2025-03-20 17:02:00 +0900

    add: solution for Baekjoon #28066

---
+++ 9주차/28066. 타노스는 요세푸스가 밉다/w9_28066_김우준.java
@@ -0,0 +1,28 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        String[] input = sc.nextLine().split(" ");
+
+        int N = Integer.parseInt(input[0]);
+        int K = Integer.parseInt(input[1]);
+
+        Queue<Integer> queue = new LinkedList<>();
+
+        for(int i = 1; i <= N; i++){
+            queue.add(i);
+        }
+
+        while(queue.size() > 1){
+            queue.add(queue.poll());
+            for(int i = 1; i < K; i++){
+                if(queue.size() == 1) break;
+                queue.poll();
+            }
+        }
+        System.out.println(queue.poll());
+    }
+}
