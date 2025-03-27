28bf3472be599ef7adafafe0be5da4c0916a4492
woojun-k <kwoojun.eng@gmail.com>
2025-03-27 16:32:08 +0900

    add: solution for Baekjoon #2156

---
+++ 10주차/2156. 포도주 시식/w10_2156_김우준.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class w10_2156_김우준 {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        
+        int N = sc.nextInt();
+        int[] score = N > 2 ? new int[N+1] : new int[3];
+        int[] sum = N > 2 ? new int[N+1] : new int[3];
+
+        for(int i = 1; i <= N; i++){
+            score[i] = sc.nextInt();
+        }
+
+        sum[1] = score[1];
+        sum[2] = score[1] + score[2];
+        for(int i = 3; i <= N; i++){
+            sum[i] = Math.max(sum[i-1], Math.max(sum[i-2] + score[i], sum[i-3] + score[i-1] + score[i]));
+        }
+
+        System.out.println(sum[N]);
+    }
+}
